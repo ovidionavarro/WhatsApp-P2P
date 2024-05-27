a8bda5e8b222966342d8fda8b566e193f11ab6bb
ovidionavarro <ovidionavarro90@gmail.com>
2024-05-27 17:33:34 -0400

    add user model and admin , migrations

--- server/polls/models.py
+++ server/polls/models.py
@@ -19,3 +19,10 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
     
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    direction = models.CharField(max_length=200)
+    last_login = models.DateTimeField("last login")
+    def __str__(self):
+        return self.username
--- server/polls/views.py
+++ server/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Question
+from .models import User
 from django.http import HttpResponse
 from django.http import Http404
 
