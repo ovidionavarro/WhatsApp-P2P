86f00bf8a3f413ea0b3b186b3a4f4e46a1cf93c7
ovidionavarro <ovidionavarro90@gmail.com>
2024-09-04 13:50:28 -0400

    fix refence,utils,codes

--- chord_node_reference.py
+++ chord_node_reference.py
@@ -9,52 +9,72 @@ from codes import *
 
 
 class ChordNodeReference:
-    def __init__(self, id: int, ip: str, port: int = 8001, m: int = 2):
+    def __init__(self, ip: str, port: int = 8001):
         self.id = getShaRepr(ip)
         self.ip = ip
         self.port = port
+
+    # Internal method to send data to the referenced node
+    def _send_data(self, op: int, data: str = None) -> bytes:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((self.ip, self.port))
+                s.sendall(f'{op},{data}'.encode('utf-8'))
+                return s.recv(1024)
+        except Exception as e:
+            logging.info(f"Error sending data: {e}")
+            return b''
+
+    # Method to find the successor of a given id
+    def find_successor(self, id: int) -> 'ChordNodeReference':
+        response = self._send_data(FIND_SUCCESSOR, str(id)).decode().split(',')
+        return ChordNodeReference(response[1], self.port)
+
+    # Method to find the predecessor of a given id
+    def find_predecessor(self, id: int) -> 'ChordNodeReference':
+        response = self._send_data(FIND_PREDECESSOR, str(id)).decode().split(',')
+        return ChordNodeReference(response[1], self.port)
+
+    # Property to get the successor of the current node
+    @property
+    def succ(self) -> 'ChordNodeReference':
+        response = self._send_data(GET_SUCCESSOR).decode().split(',')
+        return ChordNodeReference(response[1], self.port)
+
+    # Property to get the predecessor of the current node
+    @property
+    def pred(self) -> 'ChordNodeReference':
+        response = self._send_data(GET_PREDECESSOR).decode().split(',')
+        return ChordNodeReference(response[1], self.port)
+
+    # Method to notify the current node about another node
+    def notify(self, node: 'ChordNodeReference'):
+        self._send_data(NOTIFY, f'{node.id},{node.ip}')
+
+    def case_basic(self, node: 'ChordNodeReference'):
+        self._send_data(BASE, f'{node.id},{node.ip}')
+
+    # Method to check if the predecessor is alive
+    def check_predecessor(self):
+        self._send_data(CHECK_PREDECESSOR)
+
+    # Method to find the closest preceding finger of a given id
+    def closest_preceding_finger(self, id: int) -> 'ChordNodeReference':
+        response = self._send_data(CLOSEST_PRECEDING_FINGER, str(id)).decode().split(',')
+        return ChordNodeReference(response[1], self.port)
+
+    # Method to store a key-value pair in the current node
+    def store_key(self, key: str, value: str):
+        self._send_data(STORE_KEY, f'{key},{value}')
+
+    # Method to retrieve a value for a given key from the current node
+    def retrieve_key(self, key: str) -> str:
+        response = self._send_data(RETRIEVE_KEY, key).decode()
+        return response
 
     def __str__(self) -> str:
         return f'{self.id},{self.ip},{self.port}'
 
     def __repr__(self) -> str:
         return str(self)
-    #
-    # def _send_data(self, op: int, data: str = None) -> bytes:
-    #     try:
-    #         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #             s.connect((self.ip, self.port))
-    #             s.sendall(f'{op},{data}'.encode('utf-8'))
-    #             return s.recv(1024)
-    #     except Exception as e:
-    #         logging.error(f"Error sending data: {e}")
-    #         return b''
-    #
-    # def find_successor(self, id: int) -> 'ChordNodeReference':
-    #     response = self._send_data(FIND_SUCCESSOR, str(id)).decode().split(',')
-    #     return ChordNodeReference(int(response[0]), response[1], self.port)
-    #
-    # def find_predecessor(self, id: int) -> 'ChordNodeReference':
-    #     response = self._send_data(FIND_PREDECESSOR, str(id)).decode().split(',')
-    #     return ChordNodeReference(int(response[0]), response[1], self.port)
-    #
-    # @property
-    # def succ(self) -> 'ChordNodeReference':
-    #     response = self._send_data(GET_SUCCESSOR).decode().split(',')
-    #     return ChordNodeReference(int(response[0]), response[1], self.port)
-    #
-    # @property
-    # def pred(self) -> 'ChordNodeReference':
-    #     response = self._send_data(GET_PREDECESSOR).decode().split(',')
-    #     return ChordNodeReference(int(response[0]), response[1], self.port)
-    #
-    # def notify(self, node: 'ChordNodeReference'):
-    #     self._send_data(NOTIFY, f'{node.id},{node.ip}')
-    #
-    # def check_predecessor(self):
-    #     self._send_data(CHECK_PREDECESSOR)
-    #
-    # def closest_preceding_finger(self, id: int) -> 'ChordNodeReference':
-    #     response = self._send_data(CLOSEST_PRECEDING_FINGER, str(id)).decode().split(',')
-    #     return ChordNodeReference(int(response[0]), response[1], self.port)
 
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import hashlib
-from codes import M
+from codes import m
 
 
 def getShaRepr(data: str):
-    return int(hashlib.sha1(data.encode()).hexdigest(), 16)%2**M
+    return int(hashlib.sha1(data.encode()).hexdigest(), 16)%2**m
